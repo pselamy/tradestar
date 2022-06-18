cbaeb63ff8ad6ebfa522f4b1889ce83455ca6dc8
Patrick Selamy <pselamy@gmail.com>
2022-06-17 22:40:24 -0500

    Change to `CandleAggregationPipeline`.

--- src/main/java/tradestar/CandleAggregationPipeline.java
+++ src/main/java/tradestar/CandleAggregationPipeline.java
@@ -4,7 +4,7 @@ import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 
-public class CandlePipeline {
+public class CandleAggregationPipeline {
     public static void main(String[] args) {
         PipelineOptions options = PipelineOptionsFactory.fromArgs(args)
                 .withValidation()
