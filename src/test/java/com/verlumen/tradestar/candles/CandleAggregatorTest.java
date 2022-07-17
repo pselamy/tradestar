636bb74dda58c5d5fca6bb3e00678067c483da27
Patrick Selamy <pselamy@gmail.com>
2022-07-16 22:54:50 -0500

    Add `CandleAggregatorTest`.

---
+++ src/test/java/com/verlumen/tradestar/candles/CandleAggregatorTest.java
@@ -0,0 +1,57 @@
+package com.verlumen.tradestar.candles;
+
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Guice;
+import com.verlumen.tradestar.protos.candles.Candle;
+import com.verlumen.tradestar.protos.trading.ExchangeTrade;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.extensions.protobuf.ProtoCoder;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.values.PCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.util.EnumSet.allOf;
+
+@RunWith(Parameterized.class)
+public class CandleAggregatorTest {
+    private final CandleAggregator aggregator;
+    private final Pipeline pipeline;
+    private final AggregateTestCase testCase;
+
+    public CandleAggregatorTest(AggregateTestCase testCase) {
+        this.aggregator = Guice.createInjector(new CandlesModule())
+                .getInstance(CandleAggregator.class);
+        this.pipeline = TestPipeline.create();
+        this.testCase = testCase;
+    }
+
+    @Parameters
+    public static Iterable<?> data() {
+        return allOf(AggregateTestCase.class).stream()
+                .map(testCase -> new Object[]{testCase})
+                .collect(toImmutableList());
+    }
+
+    @Test
+    public void test() {
+        PCollection<ExchangeTrade> trades = pipeline
+                .apply(Create.of(testCase.trades))
+                .setCoder(ProtoCoder.of(ExchangeTrade.class));
+
+        PCollection<Candle> actual = aggregator.aggregate(trades);
+
+        PAssert.that(actual).containsInAnyOrder(testCase.expected);
+    }
+
+    private enum AggregateTestCase {
+        ;
+        private ImmutableList<ExchangeTrade> trades;
+        private ImmutableList<Candle> expected;
+    }
+}
