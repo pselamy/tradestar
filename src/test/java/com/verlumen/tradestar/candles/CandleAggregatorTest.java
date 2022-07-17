9453c2715f32c41b2dd5b4d6eba6c3e6192c9b61
Patrick Selamy <pselamy@gmail.com>
2022-07-17 12:03:19 -0500

    Update `CandleAggregatorTest`.

--- src/test/java/com/verlumen/tradestar/candles/CandleAggregatorTest.java
+++ src/test/java/com/verlumen/tradestar/candles/CandleAggregatorTest.java
@@ -3,7 +3,6 @@ package com.verlumen.tradestar.candles;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Guice;
-import com.google.mu.util.stream.BiStream;
 import com.google.testing.junit.testparameterinjector.TestParameter;
 import com.google.testing.junit.testparameterinjector.TestParameterInjector;
 import com.verlumen.tradestar.candles.CandleAggregator.AggregateParams;
@@ -22,10 +21,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Comparator;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
 @RunWith(TestParameterInjector.class)
 public class CandleAggregatorTest {
-  private final CandleAggregator aggregator;
-  private final Pipeline pipeline;
+  private CandleAggregator aggregator;
+  private Pipeline pipeline;
 
   @Before
   public void setup() {
@@ -35,39 +39,52 @@ public class CandleAggregatorTest {
 
   @Test
   public void aggregate_aggregatesCandles(
-      @TestParameter CandleAggregatorTest.AggregateAggregatesCandlesTestCase testCase) {
+      @TestParameter AggregateAggregatesCandlesTestCase testCase) {
+    // Arrange
     PCollection<ExchangeTrade> trades =
         createPCollection(testCase.trades, ProtoCoder.of(ExchangeTrade.class));
     AggregateParams params = AggregateParams.create(testCase.candles, trades);
-    BiStream.from(testCase.expected)
-        .mapValues(candles -> createPCollection(candles, ProtoCoder.of(Candle.class)));
-    PCollection<Candle> expected =
-        pipeline.apply(Create.of(testCase.expected).withCoder(ProtoCoder.of(Candle.class)));
 
+    // Act
     AggregateResult actual = aggregator.aggregate(params);
 
-    PAssert.that(actual.candles()).containsInAnyOrder(expected);
+    // Assert
+    assertThat(actual.candles()).isNotNull();
+    testCase.expected.forEach(
+        (granularity, expectedCandles) ->
+            assertThatCandlesAreAggregated(actual.candles().get(granularity), expectedCandles));
   }
 
   @Test
-  public void aggregate_throwsExceptions() {
+  public void aggregate_throwsExceptions(@TestParameter AggregateThrowsExceptionTestCase testCase) {
+    // Arrange
     PCollection<ExchangeTrade> trades =
         createPCollection(testCase.trades, ProtoCoder.of(ExchangeTrade.class));
     AggregateParams params = AggregateParams.create(testCase.candles, trades);
-    BiStream.from(testCase.expected)
-        .mapValues(candles -> createPCollection(candles, ProtoCoder.of(Candle.class)));
-    PCollection<Candle> expected =
-        pipeline.apply(Create.of(testCase.expected).withCoder(ProtoCoder.of(Candle.class)));
 
-    AggregateResult actual = aggregator.aggregate(params);
-
-    PAssert.that(actual.candles()).containsInAnyOrder(expected);
+    // Act / Assert
+    assertThrows(testCase.expectedException.getClass(), () -> aggregator.aggregate(params));
   }
 
   private <T> PCollection<T> createPCollection(ImmutableSet<T> tList, Coder<T> tCoder) {
     return pipeline.apply(Create.of(tList).withCoder(tCoder));
   }
 
+  private void assertThatCandlesAreAggregated(
+      PCollection<Candle> actual, ImmutableSet<Candle> expected) {
+    assertThat(actual).isNotNull();
+    PAssert.that(actual)
+        .satisfies(
+            actualCandles -> {
+              assertThat(actualCandles)
+                  .isInStrictOrder(
+                      Comparator.<Candle>comparingLong(candle -> candle.getStart().getSeconds()));
+              return null;
+            });
+    PAssert.that(actual).containsInAnyOrder(expected);
+  }
+
+  @SuppressWarnings("unused")
   private enum AggregateAggregatesCandlesTestCase {
     NO_CANDLES_NO_TRADES(ImmutableSet.of(), ImmutableSet.of(), ImmutableMap.of());
 
