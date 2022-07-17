362e88bed52310e11fbfeb2c7d7598411d2222e1
Patrick Selamy <pselamy@gmail.com>
2022-07-17 10:28:28 -0500

    Update `CandleAggregator` spec.

--- src/main/java/com/verlumen/tradestar/candles/CandleAggregator.java
+++ src/main/java/com/verlumen/tradestar/candles/CandleAggregator.java
@@ -1,9 +1,28 @@
 package com.verlumen.tradestar.candles;
 
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableMap;
 import com.verlumen.tradestar.protos.candles.Candle;
+import com.verlumen.tradestar.protos.candles.Granularity;
 import com.verlumen.tradestar.protos.trading.ExchangeTrade;
 import org.apache.beam.sdk.values.PCollection;
 
 public interface CandleAggregator {
-    PCollection<Candle> aggregate(PCollection<ExchangeTrade> trades);
+  AggregateResult aggregate(AggregateParams params);
+
+  @AutoValue
+  abstract class AggregateParams {
+    abstract PCollection<Candle> candles();
+
+    abstract PCollection<ExchangeTrade> trades();
+  }
+
+  @AutoValue
+  abstract class AggregateResult {
+    static AggregateResult create(PCollection<Candle> candles) {
+      return new AutoValue_CandleAggregator_AggregateResult(candles);
+    }
+
+    public abstract ImmutableMap<Granularity, PCollection<Candle>> candles();
+  }
 }
