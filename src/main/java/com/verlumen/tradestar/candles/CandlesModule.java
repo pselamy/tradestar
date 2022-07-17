78ffc14818b60118de81dae80325005eef90e675
Patrick Selamy <pselamy@gmail.com>
2022-07-17 16:14:27 -0500

    Save incomplete updates.

--- src/main/java/com/verlumen/tradestar/candles/CandlesModule.java
+++ src/main/java/com/verlumen/tradestar/candles/CandlesModule.java
@@ -1,10 +1,32 @@
 package com.verlumen.tradestar.candles;
 
+import com.google.auto.value.AutoValue;
 import com.google.inject.AbstractModule;
+import org.apache.beam.sdk.util.Sleeper;
 
 public class CandlesModule extends AbstractModule {
+  private static class BaseModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(CandleAggregator.class).to(CandleAggregatorImpl.class);
+      bind(CandleAggregator.class).to(CandleAggregatorImpl.class);
     }
+  }
+
+  @AutoValue
+  public static class ProdModule extends AbstractModule {
+    @Override
+    protected void configure() {
+      bind(Sleeper.class).toInstance(Sleeper.DEFAULT);
+
+      install(new BaseModule());
+    }
+  }
+
+  @AutoValue
+  static class TestModule extends AbstractModule {
+    @Override
+    protected void configure() {
+      install(new BaseModule());
+    }
+  }
 }
